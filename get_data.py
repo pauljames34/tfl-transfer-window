47b7e0624765a1c68c8d59b8a02942f7c8e37430
Paul James <mail@paulrichardjames.net>
2024-05-29 16:24:21 +0100

    corrent timezones

--- get_data.py
+++ get_data.py
@@ -1,4 +1,8 @@
 from datetime import datetime
+import pytz
+import requests
+from datetime import datetime, timezone
+import pytz
 import requests
 
 def get_api_data(url):
@@ -12,9 +16,16 @@ url = "https://api.tfl.gov.uk/Line/elizabeth/Arrivals/910GHANWELL?direction=inbo
 data = get_api_data(url)
 
 if data is not None:
+    current_time = datetime.now(timezone.utc)
     arrivals = [item['expectedArrival'] for item in data]
     arrivals_datetime = [datetime.fromisoformat(arrival.replace('Z', '+00:00')) for arrival in arrivals]
     arrivals_datetime.sort()
-    print(arrivals_datetime)
+    uk_tz = pytz.timezone('Europe/London')
+    time_now = datetime.now(uk_tz)
+    print(f"The time is now: {time_now.strftime('%H:%M')}")
+    for arrival in arrivals_datetime:
+        arrival_uk_time = arrival.astimezone(uk_tz)
+        if arrival_uk_time >= time_now:
+            print(arrival_uk_time.strftime('%H:%M'))
 else:
     print("No data received from API")
